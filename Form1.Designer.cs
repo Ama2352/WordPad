0f9827429e972325bd6e7cf01d0a7697e13f6258
zZDuyZz <minhduytmd06@gmail.com>
2024-11-09 11:05:18 +0700

    Add Wordpad View

---
+++ Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace WindowsFormsApp9
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.panelRuler = new System.Windows.Forms.Panel();
+            this.btnZoomIn = new System.Windows.Forms.Button();
+            this.btnZoomOut = new System.Windows.Forms.Button();
+            this.comboBoxMeasurement = new System.Windows.Forms.ComboBox();
+            this.checkBoxWordWrap = new System.Windows.Forms.CheckBox();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(19, 19);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.richTextBox1.Location = new System.Drawing.Point(90, 113);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1456, 771);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(19, 19);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 897);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1576, 26);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(151, 20);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // panelRuler
+            // 
+            this.panelRuler.Location = new System.Drawing.Point(90, 83);
+            this.panelRuler.Name = "panelRuler";
+            this.panelRuler.Size = new System.Drawing.Size(1456, 24);
+            this.panelRuler.TabIndex = 6;
+            this.panelRuler.Paint += new System.Windows.Forms.PaintEventHandler(this.panelRuler_Paint);
+            // 
+            // btnZoomIn
+            // 
+            this.btnZoomIn.Location = new System.Drawing.Point(90, 28);
+            this.btnZoomIn.Name = "btnZoomIn";
+            this.btnZoomIn.Size = new System.Drawing.Size(105, 23);
+            this.btnZoomIn.TabIndex = 7;
+            this.btnZoomIn.Text = "Zoom In";
+            this.btnZoomIn.UseVisualStyleBackColor = true;
+            this.btnZoomIn.Click += new System.EventHandler(this.btnZoomIn_Click);
+            // 
+            // btnZoomOut
+            // 
+            this.btnZoomOut.Location = new System.Drawing.Point(226, 28);
+            this.btnZoomOut.Name = "btnZoomOut";
+            this.btnZoomOut.Size = new System.Drawing.Size(106, 23);
+            this.btnZoomOut.TabIndex = 8;
+            this.btnZoomOut.Text = "Zoom Out";
+            this.btnZoomOut.UseVisualStyleBackColor = true;
+            this.btnZoomOut.Click += new System.EventHandler(this.btnZoomOut_Click);
+            // 
+            // comboBoxMeasurement
+            // 
+            this.comboBoxMeasurement.FormattingEnabled = true;
+            this.comboBoxMeasurement.Location = new System.Drawing.Point(531, 29);
+            this.comboBoxMeasurement.Name = "comboBoxMeasurement";
+            this.comboBoxMeasurement.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxMeasurement.TabIndex = 11;
+            // 
+            // checkBoxWordWrap
+            // 
+            this.checkBoxWordWrap.AutoSize = true;
+            this.checkBoxWordWrap.Location = new System.Drawing.Point(371, 31);
+            this.checkBoxWordWrap.Name = "checkBoxWordWrap";
+            this.checkBoxWordWrap.Size = new System.Drawing.Size(95, 20);
+            this.checkBoxWordWrap.TabIndex = 9;
+            this.checkBoxWordWrap.Text = "Word Wrap";
+            this.checkBoxWordWrap.UseVisualStyleBackColor = true;
+            this.checkBoxWordWrap.CheckedChanged += new System.EventHandler(this.checkBoxWordWrap_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1576, 923);
+            this.Controls.Add(this.comboBoxMeasurement);
+            this.Controls.Add(this.checkBoxWordWrap);
+            this.Controls.Add(this.btnZoomOut);
+            this.Controls.Add(this.btnZoomIn);
+            this.Controls.Add(this.panelRuler);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Wordpad";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.Panel panelRuler;
+        private System.Windows.Forms.Button btnZoomIn;
+        private System.Windows.Forms.Button btnZoomOut;
+        private System.Windows.Forms.ComboBox comboBoxMeasurement;
+        private System.Windows.Forms.CheckBox checkBoxWordWrap;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp9
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void btnZoomIn_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.ZoomFactor < 5.0f) // Giới hạn phóng to
+            {
+                richTextBox1.ZoomFactor += 0.1f; // Tăng kích thước phông chữ
+            }
+        }
+        private void btnZoomOut_Click(object sender, EventArgs e)
+        {
+            if (richTextBox1.ZoomFactor > 0.1f) // Giới hạn thu nhỏ
+            {
+                richTextBox1.ZoomFactor -= 0.1f; // Giảm kích thước phông chữ
+            }
+        }
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            int charCount = richTextBox1.Text.Length; // Số ký tự
+            int lineCount = richTextBox1.Lines.Length; // Số dòng
+            string measurementUnit = comboBoxMeasurement.SelectedItem.ToString(); // Đơn vị đo
+
+            // Cập nhật thông tin vào StatusBar
+            toolStripStatusLabel1.Text = $"Characters: {charCount}, Lines: {lineCount}, Measurement Unit: {measurementUnit}";
+        }
+        private void panelRuler_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            g.Clear(Color.White); // Làm sạch nền
+            Pen pen = new Pen(Color.Black); // Đường kẻ
+
+            float unitSize = 5f; // Đơn vị mặc định là cm
+
+            // Điều chỉnh kích thước đơn vị nếu chọn inch
+            if (comboBoxMeasurement.SelectedItem.ToString() == "Inches (in)")
+            {
+                unitSize = 2.54f; // 1 inch = 2.54 cm
+            }
+            float panelWidth = panelRuler.Width;
+
+            // Vẽ các đường kẻ trên ruler
+            for (int i = 0; i <= panelWidth; i += 10) // Vẽ mỗi 10 đơn vị
+            {
+                float x = i * unitSize; // Tính vị trí x
+                g.DrawLine(pen, x, 5, x, 20); // Đường kẻ đứng
+                g.DrawString(i.ToString(), this.Font, Brushes.Black, x - 5, 22); // Ghi số lên ruler
+            }
+
+            pen.Dispose(); // Giải phóng pen
+        }
+        private void comboBoxMeasurement_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            panelRuler.Invalidate(); // Làm mới panel để vẽ lại
+            richTextBox1_TextChanged(sender, e); // Cập nhật Status Bar
+        }
+        private void checkBoxWordWrap_CheckedChanged(object sender, EventArgs e)
+        {
+            richTextBox1.WordWrap = checkBoxWordWrap.Checked; // Bật/Tắt Word Wrap
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Thêm các đơn vị đo vào ComboBox
+            comboBoxMeasurement.Items.Add("Centimeters (cm)");
+            comboBoxMeasurement.Items.Add("Inches (in)");
+            comboBoxMeasurement.SelectedIndex = 0; // Chọn mặc định là cm
+        }
+        private void richTextBox1_SelectionChanged(object sender, EventArgs e)
+        {
+            UpdateStatusBar();
+        }
+        private void UpdateStatusBar()
+        {
+            // Lấy số dòng
+            int lineCount = richTextBox1.Lines.Length;
+            // Lấy số ký tự
+            int charCount = richTextBox1.Text.Length;
+            // Lấy số dòng hiện tại
+            int currentLine = richTextBox1.GetLineFromCharIndex(richTextBox1.SelectionStart) + 1;
+            // Tính toán số cột
+            int currentColumn = richTextBox1.SelectionStart - richTextBox1.GetFirstCharIndexFromLine(currentLine - 1) + 1;
+
+            // Cập nhật thông tin lên status bar
+            toolStripStatusLabel1.Text = $"Lines: {lineCount} | Characters: {charCount} | Line: {currentLine}, Column: {currentColumn}";
+        }
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            // Đặt chiều rộng của panelRuler để nó dài theo chiều rộng của form  
+            panelRuler.Width = this.ClientSize.Width; // Đặt chiều rộng  
+            panelRuler.Location = new Point(0, 0); // Đặt ở góc trên bên trái  
+        }
+
+    }
+}
